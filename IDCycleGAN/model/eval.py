fc6df75efc09a47e219cb6a1135e7808252340d4
Lauren Fernandes <laurenfernandes@Laurens-MacBook-Air.local>
2024-02-21 11:53:38 -0800

    IDCycleGAN Project added

---
+++ IDCycleGAN/model/eval.py
@@ -0,0 +1,63 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import numpy as np
+import facenet
+
+def computeFaceNetEmbedding(self, image_batch):
+        # Build the inference graph
+        # prelogits, _ = models.inception_resnet_v1.inference(image_batch, 0.8, phase_train=False, bottleneck_layer_size=128,
+        #                                   weight_decay=0.0)
+    prelogits, _ = models.inception_resnet_v1.inference(image_batch, 1, phase_train=False,
+                                                            bottleneck_layer_size=128, weight_decay=0.0,reuse=True)
+
+    embeddings = tf.nn.l2_normalize(prelogits, 1, 1e-10, name='embeddings')
+
+    return embeddings
+		
+def evaluate(embeddings,actual_issame,nrof_folds=10, distance_metric=0, subtract_mean=False):
+       # Calculate evaluation metrics
+    thresholds = np.arange(0, 4, 0.01)
+    embeddings1 = embeddings[0::2]
+    embeddings2 = embeddings[1::2]
+    tpr, fpr, accuracy = facenet.calculate_roc(thresholds, embeddings1, embeddings2,
+    np.asarray(actual_issame), nrof_folds=nrof_folds, distance_metric=distance_metric, subtract_mean=subtract_mean)
+    thresholds = np.arange(0, 4, 0.001)
+    val, val_std, far = facenet.calculate_val(thresholds, embeddings1, embeddings2,
+    np.asarray(actual_issame), 1e-3, nrof_folds=nrof_folds, distance_metric=distance_metric, subtract_mean=subtract_mean)
+		
+    return tpr, fpr, accuracy, val, val_std, far
+		
+def get_paths(lfw_dir, pairs):
+    nrof_skipped_pairs = 0
+    path_list = []
+    issame_list = []
+    for pair in pairs:
+        if len(pair) == 3:
+            path0 = add_extension(os.path.join(lfw_dir, pair[0], pair[0] + '_' + '%04d' % int(pair[1])))
+            path1 = add_extension(os.path.join(lfw_dir, pair[0], pair[0] + '_' + '%04d' % int(pair[2])))
+            issame = True
+        elif len(pair) == 4:
+            path0 = add_extension(os.path.join(lfw_dir, pair[0], pair[0] + '_' + '%04d' % int(pair[1])))
+            path1 = add_extension(os.path.join(lfw_dir, pair[2], pair[2] + '_' + '%04d' % int(pair[3])))
+            issame = False
+        if os.path.exists(path0) and os.path.exists(path1):    # Only add the pair if both paths exist
+            path_list += (path0,path1)
+            issame_list.append(issame)
+        else:
+            nrof_skipped_pairs += 1
+    if nrof_skipped_pairs>0:
+       print('Skipped %d image pairs' % nrof_skipped_pairs)
+    
+    return path_list, issame_list
+
+
+def read_pairs(pairs_filename):
+    pairs = []
+    with open(pairs_filename, 'r') as f:
+        for line in f.readlines()[1:]:
+            pair = line.strip().split()
+            pairs.append(pair)
+    return np.array(pairs)
